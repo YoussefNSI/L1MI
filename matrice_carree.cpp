e2a56b06dcb98638014ce0bb0f9fd6b5c0b74ffc
Youssef <90571117+YoussefNSI@users.noreply.github.com>
2022-10-03 13:27:38 +0200

    Update matrice_carree.cpp

--- matrice_carree.cpp
+++ matrice_carree.cpp
@@ -1,9 +1,11 @@
+
+
 #include <iostream>
 #include <vector>
 
 int main()
 {
-    int taille, num;
+    int taille, num, aux;
     std::cout<< "Entrez la taille de la matrice carrée";
     std::cin>> taille;
     std::vector<std::vector<int>> matrice(taille, std::vector<int>(taille));
@@ -15,5 +17,17 @@ int main()
             std::cout<< "matrice["<<i<<"]["<<j<<"] = "<< matrice[i][j] << "\n";
         }
     }
+    
+    std::cout<<"transposée : ";
+    
+    for(int i = 0; i <= taille; i++){
+        for(int j=0; j <= taille; j++){
+            aux = matrice[i][j];
+            matrice[i][j] = matrice[j][i];
+            matrice[j][i] = aux;
+            std::cout<< "matrice["<<i<<"]["<<j<<"] = "<< matrice[i][j] << "\n";
+        }
+    }
+    
     return 0;
 }
