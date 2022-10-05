f739efc61249293e91eb74c85bb62d77db2a45f6
Youssef <90571117+YoussefNSI@users.noreply.github.com>
2022-10-05 10:36:17 +0200

    fin feuille tp

--- TP 1/bataille_navale.cpp
+++ TP 1/bataille_navale.cpp
@@ -13,25 +13,26 @@ int main(){
     std::cout<<"Nombre de bateaux : ";
     std::cin>>nb_bateau;
     int tab_bateau[nb_bateau] = {};
-    for(int i = 0; i < nb_bateau; i++){
+    for(int i = 0; i <= nb_bateau-1; i++){
         std::cout<< "Longueur du bateau no."<<i<<" : ";
         std::cin>>long_bateau;
         tab_bateau[i] = long_bateau;
     }
-    for(int i = 0; i <= nb_bateau; i++){
+    for(int i = 0; i <= nb_bateau-1; i++){
         bool horizontal = false;
         bool place_trouve = false;
         bool placer = false;
         int boucle_i = i;
         while(not placer){
+            std::cout<<"\nje passe dans la boucle pour i = " << i << "\n";
             while(not place_trouve){
-                position_x = (rand() % 9);
-                position_y = (rand() % 9);
+                position_x = (rand() % 10);
+                position_y = (rand() % 10);
                 std::cout << "position x : " << position_x << " position y : " << position_y << "\n";
-                if(9 - position_x < 0 or position_x - tab_bateau[i] < 0){
-                    if((9 - position_y) < 0 or (position_y - tab_bateau[i]) < 0){
-                        //depasse la grille a l'horizontal
-                        std::cout<< "Le bateau depasse, longueur = " << tab_bateau[i];
+                if(tab_bateau[i] > abs((position_x - 9))){
+                    if(tab_bateau[i] > abs((position_y - 9))){
+                        //depasse la grille a la verticale
+                        std::cout<< "Le bateau depasse, longueur = " << tab_bateau[i] << "\n";
                         ;
                     }
                     else{
@@ -46,33 +47,49 @@ int main(){
             }
         
             if(horizontal){
-                std::cout<< "bateau a l'horizontal";
+                std::cout<< "bateau a l'horizontal\n";
                 //bateau a l'horizontal
+                int position_y_test = position_y;
                 placer = true;
                 for(int k = 0; k < tab_bateau[boucle_i]; k++){
-                    if(grille[position_x][position_y] != 0){
+                    if(grille[position_x][position_y_test] != 0){
                         placer = false;
+                        place_trouve = false;
                     }
-                    else{
+                    else position_y_test += 1;                   
+                }
+                if(placer){
+                    for(int k = 0; k < tab_bateau[boucle_i]; k++){
                         grille[position_x][position_y] = tab_bateau[boucle_i];
+                        std::cout << "position x : " << position_x << " position y : " << position_y << "\n";
+                        position_y += 1;           
                     }
-                    std::cout << "position x : " << position_x << " position y : " << position_y << "\n";
-                    position_y += 1;
-                    
                 }
             }
             else{
-                std::cout<<"bateau a la verticale";
+                std::cout<<"bateau a la verticale\n";
                 //bateau a la verticale
-                for(int k = 0; k < tab_bateau[boucle_i]; k++){
-                    grille[position_x][position_y] = tab_bateau[boucle_i];
-                    std::cout << "position x : " << position_x << " position y : " << position_y << "\n";
-                    position_x += 1;
-                }
+                int position_x_test = position_x;
                 placer = true;
+                for(int k = 0; k < tab_bateau[boucle_i]; k++){
+                    if(grille[position_x_test][position_y] != 0){
+                        placer = false;
+                        place_trouve = false;
+                    }
+                    else position_x_test += 1;
+                }
+                if(placer){
+                    for(int k = 0; k < tab_bateau[boucle_i]; k++){
+                        grille[position_x][position_y] = tab_bateau[boucle_i];
+                        std::cout << "position x : " << position_x << " position y : " << position_y << "\n";
+                        position_x += 1;
+                    }
+                }
             }
         }   
     }
+    std::cout<<"Grille finale : \n \n";
+
     for(int i = 0; i <= 9; i++){
         std::cout<<"\n";
         for(int j = 0; j <= 9; j++){
